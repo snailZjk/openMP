174f2b2968260b19dc0bb8faf72480be04d7c066
snailZjk <Zjk@users.noreply.github.com>
2019-12-03 21:19:06 +0800

    项目创建

---
+++ Dijkstra/Dijkstra.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+	clock_t start = clock();
+
+#pragma omp parallel for num_threads(1)
+	for (int i = 0; i < 10000; i++) {
+		for (int j = 0; j < 100000; j++) {
+			int i = 10;
+			i++;
+		}
+	}
+	clock_t end = clock();
+	cout << "time: " << end - start << " ms" << endl;
+}
